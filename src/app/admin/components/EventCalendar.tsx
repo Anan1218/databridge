b4e6669d09b6c50ab7dce3a53b0a8ab0801ef0e7
Anan Wang <mengan.wang@gmail.com>
2025-01-31 19:22:54 -0800

    update calendar stuff

--- src/app/admin/components/EventCalendar.tsx
+++ src/app/admin/components/EventCalendar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar, momentLocalizer, View } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useState } from 'react';
@@ -9,15 +9,21 @@ const localizer = momentLocalizer(moment);
 
 export default function EventCalendar() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [currentView, setCurrentView] = useState('month');
+  const [currentView, setCurrentView] = useState<View>('month');
   const events = [
     {
       id: 1,
       title: 'Sample Event',
-      start: new Date(2024, 1, 12, 10, 0),
-      end: new Date(2024, 1, 12, 12, 0),
+      start: new Date(2025, 0, 12, 10, 0),
+      end: new Date(2025, 1, 12, 12, 0),
     },
-    // Add more events here
+    {
+      id: 2,
+      title: 'Happy Hour',
+      start: new Date(2025, 0, 31, 17, 0, 0, 0),
+      end: new Date(2025, 0, 31, 17, 30, 0, 0),
+      desc: 'Most important meal of the day',
+    },
   ];
 
   return (
