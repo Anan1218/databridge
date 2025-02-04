6e1194230fbd69dd183a49108eb310b8bd37adc9
Anan Wang <mengan.wang@gmail.com>
2025-02-03 19:52:52 -0800

    change book demo to not use unused nodemailer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AuthContextProvider } from '@/contexts/AuthContext';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "ProspectAI",
-  description: "Connect, monitor, and analyze data with AI",
+  description: "The best way to generate leads with AI",
   icons: {
     icon: '/prospectailogo.png',
     apple: '/prospectailogo.png',
@@ -18,6 +19,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-WCG2YRWT2T"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WCG2YRWT2T');
+          `}
+        </Script>
+      </head>
       <body>
         <AuthContextProvider>
           {children}
